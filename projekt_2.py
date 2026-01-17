55882b9e23637e99b88259d8b69eff7a836f1d30
fff2da <pxlafs@gmail.com>
2026-01-17 14:17:30 +0100

    Create projekt_2.py

---
+++ projekt_2.py
@@ -0,0 +1,65 @@
+"""
+projekt_2.py: druhý projekt do Engeto Online Python Akademie
+
+author: tvoje jmeno
+email: tvuj@email.cz
+discord: neuvedeno
+"""
+
+import random
+
+print("Hi there!")
+print("-----------------------------------------------")
+print("I've generated a random 4 digit number for you.")
+print("Let's play a bulls and cows game.")
+print("-----------------------------------------------")
+
+numbers = list("0123456789")
+random.shuffle(numbers)
+
+if numbers[0] == "0":
+    numbers[0], numbers[1] = numbers[1], numbers[0]
+
+secret_number = "".join(numbers[:4])
+
+guesses = 0
+
+while True:
+    guess = input("Enter a number: ")
+
+    if not guess.isdigit():
+        print("Please enter only numbers.")
+        continue
+
+    if len(guess) != 4:
+        print("Number must have 4 digits.")
+        continue
+
+    if guess[0] == "0":
+        print("Number cannot start with 0.")
+        continue
+
+    if len(set(guess)) != 4:
+        print("Digits must be unique.")
+        continue
+
+    guesses += 1
+
+    bulls = 0
+    cows = 0
+
+    for i in range(4):
+        if guess[i] == secret_number[i]:
+            bulls += 1
+        elif guess[i] in secret_number:
+            cows += 1
+
+    if bulls == 4:
+        print("-----------------------------------------------")
+        print("Correct, you've guessed the right number")
+        print(f"in {guesses} guesses!")
+        print("-----------------------------------------------")
+        break
+    else:
+        print(f"{bulls} bulls, {cows} cows")
+        print("-----------------------------------------------")
